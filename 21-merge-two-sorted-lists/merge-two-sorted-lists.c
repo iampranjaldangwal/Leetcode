8e6b63249e64ec9b4901260b918bfc95ecb2e23d
Pranjal Dangwal <118796002+iampranjaldangwal@users.noreply.github.com>
2023-10-05 08:57:07 +0530

    Time: 1 ms (76.18%) | Memory: 6.7 MB (31.32%) - LeetSync

---
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.c
@@ -0,0 +1,58 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2){
+       struct ListNode* head=NULL;
+       struct ListNode* tail=NULL;
+       if(list1==NULL)
+       return list2;
+       if(list2==NULL)
+       return list1;
+       while(list1!=NULL && list2!=NULL){
+           if(head==NULL && tail==NULL){
+               if(list1->val<list2->val){
+                   head=list1;
+                   tail=list1;
+                   list1=list1->next;
+               }
+               else{
+                   head=list2;
+                   tail=list2;
+                   list2=list2->next;
+               }
+           }
+        else{
+           if(list1->val > list2->val){
+                   tail->next=list2;
+                   tail=tail->next;
+                   list2=list2->next;
+               }
+               else{
+                   tail->next=list1;
+                   tail=tail->next;
+                   list1=list1->next;
+               }
+            }
+         }
+      
+        if(list1!=NULL){
+        while(list1!=NULL){
+           tail->next=list1;
+           tail=list1;
+           list1=list1->next;
+           }
+       }
+
+           if(list2!=NULL){
+           while(list2!=NULL){
+           tail->next=list2;
+           tail=list2;
+           list2=list2->next;
+           }
+        }   
+        return head;
+}
