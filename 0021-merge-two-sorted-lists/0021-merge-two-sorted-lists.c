dc70dc5a25a11dc365785552fb06909f88a92f31
Pranjal Dangwal <118796002+iampranjaldangwal@users.noreply.github.com>
2023-10-05 08:56:44 +0530

    Time: 1 ms (76.18%), Space: 6.7 MB (31.32%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.c
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.c
@@ -6,23 +6,53 @@
  * };
  */
 struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2){
-       struct ListNode* dummy=(struct ListNode*)malloc(sizeof(struct ListNode));
-       dummy->next=NULL;
-     struct ListNode* temp=dummy;
-    while(list1!=NULL && list2!=NULL){
-        if(list1->val <= list2->val){
-            temp->next=list1;
-            list1=list1->next;
-        }
+       struct ListNode* head=NULL;
+       struct ListNode* tail=NULL;
+       if(list1==NULL)
+       return list2;
+       if(list2==NULL)
+       return list1;
+       while(list1!=NULL && list2!=NULL){
+           if(head==NULL && tail==NULL){
+               if(list1->val<list2->val){
+                   head=list1;
+                   tail=list1;
+                   list1=list1->next;
+               }
+               else{
+                   head=list2;
+                   tail=list2;
+                   list2=list2->next;
+               }
+           }
         else{
-             temp->next=list2;
-            list2=list2->next;
-        }
-        temp=temp->next;
-    }
-    if(list1!=NULL)
-        temp->next=list1;
-    if(list2!=NULL)
-        temp->next=list2;
-   return dummy->next; 
+           if(list1->val > list2->val){
+                   tail->next=list2;
+                   tail=tail->next;
+                   list2=list2->next;
+               }
+               else{
+                   tail->next=list1;
+                   tail=tail->next;
+                   list1=list1->next;
+               }
+            }
+         }
+      
+        if(list1!=NULL){
+        while(list1!=NULL){
+           tail->next=list1;
+           tail=list1;
+           list1=list1->next;
+           }
+       }
+
+           if(list2!=NULL){
+           while(list2!=NULL){
+           tail->next=list2;
+           tail=list2;
+           list2=list2->next;
+           }
+        }   
+        return head;
 }
