964335e38ba06784cfa722e50e5ece5ee6f56d77
Pranjal Dangwal <118796002+iampranjaldangwal@users.noreply.github.com>
2024-05-04 17:02:51 +0530

    Time: 58 ms (54.14%), Space: 45.5 MB (8.97%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.cpp
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -1,27 +1,18 @@
+#pragma GCC optimize("O3", "unroll-loops")
 class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
-        sort(people.begin(),people.end());
-        int n = people.size();
-        int minBoats = 0;
-        int i=0,j=n-1;
 
-           while(i<j){
-                if(people[j] >= limit){
-                 minBoats++;
-                 j--;
-                }
-                if((people[i]+people[j] <= limit) ){
+        sort(people.begin(),people.end());
+
+        int minBoats = 0;
+        int i=0,j=people.size()-1;
+            
+            while(i<=j){
                 minBoats++;
+                if(people[i]+people[j] <= limit)
                 i++;
                 j--;
-                }
-                else if(people[i]+people[j] >= limit){
-                    minBoats++;
-                    j--;
-                }
-                if(i==j)
-              minBoats++;
             }
      return minBoats;
     }
