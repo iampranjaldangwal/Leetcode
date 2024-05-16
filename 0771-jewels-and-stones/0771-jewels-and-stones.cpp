977081234e286d057e42028bc90669af1a9415b7
Pranjal Dangwal <118796002+iampranjaldangwal@users.noreply.github.com>
2024-05-16 21:21:06 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (26.85%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.cpp
+++ 0771-jewels-and-stones/0771-jewels-and-stones.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     int numJewelsInStones(string jewels, string stones) {
-        map<char,int> mp;
-        int n = jewels.size();
-        int m = stones.size();
+        unordered_map<char, int> mp;
+        
+        for (char jewel : jewels) {
+            mp[jewel] = 0; 
+        }
+        
         int count = 0;
-        for(int i=0;i<m;i++)
-           mp[stones[i]]++;
-        for(int i=0;i<n;i++)
-           count += mp[jewels[i]];
+        for (char stone : stones) {
+            if (mp.find(stone) != mp.end())
+             count++; 
+        }    
         return count;
     }
 };
