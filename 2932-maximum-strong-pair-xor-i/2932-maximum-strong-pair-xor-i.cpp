78645f86aedf5f1bc9fba526218d5891f09d9b7e
Pranjal Dangwal <118796002+iampranjaldangwal@users.noreply.github.com>
2024-05-22 01:26:59 +0530

    Time: 5 ms (71.44%), Space: 29.1 MB (5.99%) - LeetHub

--- 2932-maximum-strong-pair-xor-i/2932-maximum-strong-pair-xor-i.cpp
+++ 2932-maximum-strong-pair-xor-i/2932-maximum-strong-pair-xor-i.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     int maximumStrongPairXor(vector<int>& nums) {
-        int n = nums.size();
         int maxXor = 0;
-        for(int i=0;i<n;i++){
-            int x = nums[i];
-            for(int j=0;j<n;j++){
-                int y = nums[j];
+        unordered_set<int> s(nums.begin(),nums.end());
+        for(auto& x:s){
+            for(auto& y:s){
                 if(abs(x-y) <= min(x,y))
                 maxXor = max(maxXor,(x^y));
             }
