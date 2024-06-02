daecda1ec3ee12ea7b426a67c600d2082e377476
Pranjal Dangwal <118796002+iampranjaldangwal@users.noreply.github.com>
2024-06-02 14:14:44 +0530

    Time: 20 ms (43.37%), Space: 8.7 MB (7.91%) - LeetHub

--- 1742-maximum-number-of-balls-in-a-box/1742-maximum-number-of-balls-in-a-box.cpp
+++ 1742-maximum-number-of-balls-in-a-box/1742-maximum-number-of-balls-in-a-box.cpp
@@ -2,9 +2,6 @@ class Solution {
 public:
     int countBalls(int lowLimit, int highLimit) {
         unordered_map<int,int> mp;
-        for(int i=1;i<=highLimit;i++)
-        mp[i] = 0;
-
         for(int i=lowLimit;i<=highLimit;i++){
             int num = i,sum=0;
             while(num>0){
