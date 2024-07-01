419b6102a5f60f0ed6f6cc96129d01619a723d0d
Pranjal Dangwal <118796002+iampranjaldangwal@users.noreply.github.com>
2024-07-01 10:39:04 +0530

    Time: 3 ms (60.88%), Space: 10.6 MB (45.76%) - LeetHub

--- 1550-three-consecutive-odds/1550-three-consecutive-odds.cpp
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.cpp
@@ -3,8 +3,9 @@ public:
     bool threeConsecutiveOdds(vector<int>& arr) {
         int n = arr.size();
         for(int i=0;i<n-2;i++){
-          if(arr[i]%2 != 0 && arr[i+1]%2 != 0 && arr[i+2]%2 !=0)
-          return true;
+            int product = arr[i] * arr[i+1] * arr[i+2];
+            if(product % 2 != 0)
+             return true;
         }
         return false;
     }
